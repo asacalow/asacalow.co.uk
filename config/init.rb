ce10237aa1a79d797cd4e0cf36eda763a1f15b75
Asa Calow <asacalow@asa-calows-macbook-pro.local>
2008-05-09 00:35:54 +0100

    Transferred to a non-flat merb app

--- config/init.rb
+++ config/init.rb
@@ -1,22 +1,24 @@
-# Move this to application.rb if you want it to be reloadable in dev mode.
-Merb::Router.prepare do |r|
-  r.match('/').to(:controller => 'main', :action =>'index')
-  r.default_routes
-end
+# Make the app's "gems" directory a place where gems are loaded from
+Gem.clear_paths
+Gem.path.unshift(Merb.root / "gems")
+
+# Make the app's "lib" directory a place where ruby files get "require"d from
+$LOAD_PATH.unshift(Merb.root / "lib")
 
 
-Merb::Config.use { |c|
-  c[:environment]         = 'production',
-  c[:framework]           = {},
-  c[:log_level]           = 'debug',
-  c[:use_mutex]           = false,
-  c[:session_store]       = 'cookie',
-  c[:session_id_key]      = '_session_id',
-  c[:session_secret_key]  = 'bd81860e54a874b7f87173cb89e5b87211661349',
-  c[:exception_details]   = true,
-  c[:reload_classes]      = true,
-  c[:reload_time]         = 0.5
-}
+Merb::Config.use do |c|
+  
+  ### Sets up a custom session id key, if you want to piggyback sessions of other applications
+  ### with the cookie session store. If not specified, defaults to '_session_id'.
+  # c[:session_id_key] = '_session_id'
+  
+  c[:session_secret_key]  = '539745e52ac60fc4705caa9b9261ce267ac16dc7'
+  c[:session_store] = 'cookie'
+end  
+
+use_test :rspec
+
+### Add your other dependencies here
 
 dependency "merb_helpers"
 
@@ -25,6 +27,7 @@ Merb::BootLoader.after_app_loads do
   require "twitter"
   require "yaml"
   require "memcached"
+  require "redcloth"
   
   # set up my memcached server here...
   $cache = Memcached.new("127.0.0.1:11211")
--- spec/controllers/main_spec.rb
+++ spec/controllers/main_spec.rb
@@ -2,24 +2,6 @@ require File.join(File.dirname(__FILE__), "..", 'spec_helper.rb')
 
 describe Main, "index action" do
   before(:each) do
-    @post = mock("Twitter::Status")
-    Twitter::Base.stub!(:timeline).and_return([@post])
-    Memcached.stub!(:get).and_return(@post)
-    Memcached.stub!(:set)
-  end
-  
-  def do_get
-    dispatch_to(Main, :index) do |controller|
-      controller.stub!(:display)
-    end
-  end
-  
-  it "should handle an error from the Twitter API" do
-    Twitter::Base.any_instance.should_receive(:timeline).and_raise(Twitter::CantConnect)
-    do_get.should be_successful
-  end
-  
-  it "should handle an error from Memcached" do
-    do_get.should be_successful
+    dispatch_to(Main, :index)
   end
 end
