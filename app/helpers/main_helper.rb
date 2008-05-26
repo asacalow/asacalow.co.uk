9a32366782fa74562b7ba20814a510f561ee6af3
Asa Calow <asacalow@asa-calows-macbook-pro.local>
2008-05-26 20:02:58 +0100

    Swapped out textillisation for a more twitter-friendly linkification

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,9 +1,8 @@
 module Merb
   module MainHelper
-    def textilise(string)
-      string = CGI.unescapeHTML string
-      r = RedCloth.new(string)
-      return r.to_html
+    def linkify(string)
+      regex = Regexp.new '(https?:\/\/([-\w\.]+)+(:\d+)?(\/([\w\/_\.]*(\?\S+)?)?)?)'
+      string.gsub( regex, '<a href="\1">\1</a>' )
     end
   end
 end
