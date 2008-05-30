0b201a262b6344e207206fcde9b0c85d53376a58
Asa Calow <mothership@asacalow.co.uk>
2008-05-30 11:04:01 +0100

    Fleshed out the specs

--- spec/controllers/main_spec.rb
+++ spec/controllers/main_spec.rb
@@ -2,13 +2,18 @@ require File.join(File.dirname(__FILE__), "..", 'spec_helper.rb')
 
 describe Main, "index action" do
   before(:each) do
-    @mock_cache = mock('Memcached')
-    @mock_cache.stub!(:get).and_return([])
-    @mock_cache.stub!(:set)
-    Memcached.stub!(:new).and_return(@mock_cache)
+    @mock_posts = [
+      mock('Post1', :text => 'Ding Dong', :created_at => DateTime.now.to_date),
+      mock('Post2', :text => 'Ping Pong', :created_at => DateTime.now.to_date),
+      mock('Post3', :text => 'King Kong', :created_at => DateTime.now.to_date)
+    ]
+    
+    $cache = mock('Memcached')
+    $cache.stub!(:get).and_return(@mock_posts)
+    $cache.stub!(:set)
     
     @mock_twitter = mock('Twitter::Base')
-    @mock_twitter.stub!(:timeline).and_return([])
+    @mock_twitter.stub!(:timeline).and_return(@mock_posts)
     Twitter::Base.stub!(:new).and_return(@mock_twitter)
   end
   
@@ -19,22 +24,24 @@ describe Main, "index action" do
   end
   
   it "should display some tweets from cache" do
-    @mock_cache.should_receive(:get).once.and_return([])
+    $cache.should_receive(:get).once.and_return(@mock_posts)
     do_get.should respond_successfully
   end
   
   it "should reset the cache from Twitter" do
-    @mock_cache.stub!(:get).and_raise(Memcached::NotFound)
+    $cache.stub!(:get).and_raise(Memcached::NotFound)
+    $cache.should_receive(:set).once
+    @mock_twitter.should_receive(:timeline).once.and_return(@mock_posts)
     do_get.should respond_successfully
   end
   
   it "should cope with Memcached timeouts" do
-    @mock_cache.stub!(:get).and_raise(Memcached::ATimeoutOccurred)
+    $cache.stub!(:get).and_raise(Memcached::ATimeoutOccurred)
     do_get.should respond_successfully
   end
   
   it "should cope with Twitter API timeouts" do
-    @mock_cache.stub!(:get).and_raise(Memcached::NotFound)
+    $cache.stub!(:get).and_raise(Memcached::NotFound)
     Twitter::Base.stub!(:new).and_raise(Twitter::CantConnect)
     do_get.should respond_successfully
   end
